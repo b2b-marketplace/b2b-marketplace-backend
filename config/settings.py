65d985ed23d19b365915894e3525f96e5dd3f52e
bulatgabdullin <112638878+ByJIaT@users.noreply.github.com>
2023-08-06 10:57:44 +0500

    Fix env path

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "../infra/.env"))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
