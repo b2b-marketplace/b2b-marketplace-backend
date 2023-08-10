2bd10f489f2bee83f6376cf22e40d608f21b3ada
Илья Росманов <rosmanov2012@yandex.ru>
2023-08-10 20:24:59 +0300

    сделан перевод

--- apps/baskets/models.py
+++ apps/baskets/models.py
@@ -3,13 +3,11 @@ from django.db import models
 
 
 class Basket(models.Model):
-    user = models.ForeignKey(
-        get_user_model(), on_delete=models.CASCADE, related_name="user", verbose_name="Пользователь"
-    )
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="user", verbose_name="User")
 
     class Meta:
-        verbose_name = "Корзина"
-        verbose_name_plural = "Корзины"
+        verbose_name = "Basket"
+        verbose_name_plural = "Baskets"
 
     def __str__(self):
         return f"{self.user}"
@@ -17,12 +15,12 @@ class Basket(models.Model):
 
 class BasketProduct(models.Model):
     basket = models.OneToOneField(Basket, on_delete=models.CASCADE)
-    product = models.ForeignKey("Product", on_delete=models.CASCADE, related_name="product", verbose_name="Товар")
-    quantity = models.PositiveIntegerField(related_name="product quantity", verbose_name="Количество товара")
+    product = models.ForeignKey("Product", on_delete=models.CASCADE, related_name="product", verbose_name="Product")
+    quantity = models.PositiveIntegerField(related_name="product quantity", verbose_name="Product quantity")
 
     class Meta:
-        verbose_name = "Товар в корзине"
-        verbose_name_plural = "Товары в корзине"
+        verbose_name = "Product in the basket"
+        verbose_name_plural = "Products in the basket"
 
     def __str__(self):
         return f"{self.quantity} x {self.product} in Basket for User: {self.basket.user}"
