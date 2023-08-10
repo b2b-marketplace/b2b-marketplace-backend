405c6e3c48ba4e19ba22423e79fdb38cc8e898f1
bulatgabdullin <112638878+ByJIaT@users.noreply.github.com>
2023-08-10 18:37:58 +0500

    Add order app, order model, oprderproduct model, order admin

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ THIRD_PARTY_APPS = [
     "rest_framework",
 ]
 
-LOCAL_APPS = ["apps.core", "apps.products"]
+LOCAL_APPS = ["apps.core", "apps.products", "apps.orders"]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
