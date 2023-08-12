16ea1be15fcce29390e7302c768adeeea448e1fd
Ivan Skvortsov <pprofcheg@gmail.com>
2023-08-12 20:50:13 +0300

    added page number pagination

--- config/settings.py
+++ config/settings.py
@@ -135,6 +135,8 @@ if DEBUG:
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 20,
 }
 
 SPECTACULAR_SETTINGS = {
