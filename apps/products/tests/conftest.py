5d2ee1de3e86df2e977c7b4498398d07e11a8641
Ivan Skvortsov <pprofcheg@gmail.com>
2023-08-14 11:23:44 +0300

    removed print from django_db_setup

--- apps/products/tests/conftest.py
+++ apps/products/tests/conftest.py
@@ -8,7 +8,6 @@ from apps.users.models import CustomUser
 def django_db_setup():
     from django.conf import settings
 
-    print(settings.DATABASES)
     settings.DATABASES["default"] = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": settings.BASE_DIR / "db.sqlite3",
