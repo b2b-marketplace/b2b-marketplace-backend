ba5f094116d0f88ca2b22813629a9d9321976223
bulatgabdullin <112638878+ByJIaT@users.noreply.github.com>
2023-08-14 20:58:04 +0500

    Add get_serializer_class get_serializer_class

--- apps/users/views.py
+++ apps/users/views.py
@@ -5,25 +5,33 @@ from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
-from apps.users.serializers import CompanyCreateSerializer
+from apps.users.serializers.companies import CompanySerializer, UserReadSerializer
 
 User = get_user_model()
 
 
 class CustomUserViewSet(UserViewSet):
+    def get_serializer_class(self):
+        if self.action == "companies":
+            return UserReadSerializer
+        elif self.action == "create_company":
+            return CompanySerializer
+        else:
+            return super().get_serializer_class()
+
     @action(("get",), detail=False, permission_classes=(AllowAny,))
     def companies(self, request):
         user = User.objects.get_companies()
         page = self.paginate_queryset(user)
         if page is not None:
-            serializer = CompanyCreateSerializer(page, many=True)
+            serializer = self.get_serializer(page, many=True)
             return self.get_paginated_response(serializer.data)
-        serializer = CompanyCreateSerializer(user, many=True)
+        serializer = self.get_serializer(user, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     @companies.mapping.post
     def create_company(self, request):
-        serializer = CompanyCreateSerializer(data=request.data)
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
