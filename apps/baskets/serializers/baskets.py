30b3c8a7c8c55f22a36be13a8e05c7494027c7c4
Ольга Ротбардт <knyajna.tarakanova@gmail.com>
2023-08-17 17:28:55 +0400

    Валидация одинаковых товаров в корзине

--- apps/baskets/serializers/baskets.py
+++ apps/baskets/serializers/baskets.py
@@ -1,13 +1,11 @@
+# from django.core.exceptions import ValidationError
 # from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-from apps.baskets.models import Basket
+from apps.baskets.models import Basket, BasketProduct
+from apps.users.models import CustomUser
 
-from .basketproducts import BasketProductReadSerializer
-
-# , BasketProduct
-
-# , BasketProductWriteSerializer
+from .basketproducts import BasketProductReadSerializer, BasketProductWriteSerializer
 
 # User = get_user_model()
 
@@ -32,37 +30,58 @@ class BasketReadSerializer(serializers.ModelSerializer):
 class BasketWriteSerializer(serializers.ModelSerializer):
     """Сериализатор модели Basket.
 
-    Используется для создания корзины.
+    Используется для добавления товаров в корзину.
     """
 
     # TODO: заменить на кастомный сериализатор пользователя
     user = serializers.PrimaryKeyRelatedField(read_only=True)
-    # basket_products = BasketProductWriteSerializer(many=True)
+    basket_products = BasketProductWriteSerializer(many=True)
 
     class Meta:
         model = Basket
         fields = "__all__"
 
-    # def create_products_quantity(self, basket_products, basket):
-    #     for product in basket_products:
-    #         BasketProduct.objects.create(
-    #             basket=basket,
-    #             product_id=product['id'],
-    #             quantity=product['quantity'])
+    def validate(self, data):
+        basket_products = data.get("basket_products", [])
+        if basket_products:
+            product_id = basket_products[0]["id"]
+            self.validate_products(product_id)
+        return data
 
-    # def create(self, validated_data):
-    #     basket_products = validated_data.pop('basket_products')
-    #     basket = Basket.objects.create(**validated_data)
-    #     self.create_products_quantity(basket_products, basket)
-    #     return basket
+    def validate_products(self, product_id):
+        user = CustomUser.objects.get(id=1)
+        basket_products = BasketProduct.objects.filter(basket__user=user)
+        product_ids = set(basket_products.values_list("product_id", flat=True))
 
-    # def update(self, basket, validated_data):
-    #     basket_products = validated_data.pop('basket_products')
-    #     basket.basket_products.clear()
-    #     self.create_products_quantity(basket_products, basket)
-    #     return super().update(basket, validated_data)
+        if product_id in product_ids:
+            raise serializers.ValidationError("Product already exists in the basket.")
 
-    # def to_representation(self, basket):
-    #     representation = BasketReadSerializer(
-    #         basket, context={'request': self.context.get('request')})
-    #     return representation.data
+    def create_or_update_basket(self, user, instance=None):
+        user = CustomUser.objects.get(id=1)
+        if instance:
+            return instance
+        basket, created = Basket.objects.get_or_create(user=user)
+        return basket
+
+    def create_products_quantity(self, basket_products, basket):
+        for product in basket_products:
+            BasketProduct.objects.create(
+                basket=basket, product_id=product["id"], quantity=product["quantity"]
+            )
+
+    def create(self, validated_data):
+        basket_products = validated_data.pop("basket_products")
+        basket = self.create_or_update_basket(validated_data)
+        self.create_products_quantity(basket_products, basket)
+        return basket
+
+    def update(self, instance, validated_data):
+        basket_products = validated_data.pop("basket_products")
+        self.create_products_quantity(basket_products, instance)
+        return instance
+
+    def to_representation(self, basket):
+        representation = BasketReadSerializer(
+            basket, context={"request": self.context.get("request")}
+        )
+        return representation.data
--- apps/baskets/views.py
+++ apps/baskets/views.py
@@ -1,15 +1,23 @@
-from rest_framework import permissions, status, viewsets
-from rest_framework.decorators import action
-from rest_framework.response import Response
+from rest_framework import permissions, viewsets
 
 from apps.baskets.models import Basket
+
+# , BasketProduct, Product
 from apps.baskets.serializers.baskets import BasketReadSerializer, BasketWriteSerializer
+
+# from apps.baskets.serializers.basketproducts import BasketProductWriteSerializer
 from apps.users.models import CustomUser
+
+# from rest_framework.decorators import action
+# from rest_framework.response import Response
 
 
 class BasketViewSet(viewsets.ModelViewSet):
-    queryset = Basket.objects.all().order_by("-id")
-    http_method_names = ["get", "post", "delete"]
+    # http_method_names = ["get", "post", "delete"]
+
+    def get_queryset(self):
+        user = CustomUser.objects.get(id=1)
+        return Basket.objects.filter(user=user)
 
     def get_serializer_class(self):
         if self.request.method in permissions.SAFE_METHODS:
@@ -17,17 +25,5 @@ class BasketViewSet(viewsets.ModelViewSet):
         return BasketWriteSerializer
 
     def perform_create(self, serializer):
-        # TODO: заглушка, пока не реализована аутентификация
-        # После реализации аутентификации, взять юзера из request (self.request.user)
-        user = CustomUser.objects.filter(is_company=True).first()
+        user = CustomUser.objects.get(id=1)
         serializer.save(user=user)
-
-    @action(detail=False, methods=["POST"])
-    def create_and_redirect(self, request):
-        return Response(
-            status=status.HTTP_201_CREATED, headers={"Location": self.reverse_action("mine")}
-        )
-
-    @action(detail=False, methods=["get"], url_path="mine")
-    def my_basket(self, request):
-        return Response({"message": "Моя корзина"})
