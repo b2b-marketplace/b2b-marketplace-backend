c72ab6c8ee59b292c4177f90b719e0f28daacf3c
bulatgabdullin <112638878+ByJIaT@users.noreply.github.com>
2023-08-19 21:04:07 +0500

    add destroy method CustomUserViewSet

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
+from djoser import utils
 from djoser.views import UserViewSet
-from drf_spectacular.utils import OpenApiExample, extend_schema
+from drf_spectacular.utils import extend_schema
 from rest_framework import permissions, status
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
@@ -39,6 +40,13 @@ class CustomUserViewSet(UserViewSet):
 
         return super().get_serializer_class()
 
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance == request.user:
+            utils.logout_user(self.request)
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
     @extend_schema(responses={200: UserCompanyReadSerializer(many=True)})
     @action(("get",), detail=False, permission_classes=(AllowAny,))
     def companies(self, request):
@@ -60,34 +68,9 @@ class CustomUserViewSet(UserViewSet):
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     @extend_schema(
-        examples=[
-            OpenApiExample(
-                name="response",
-                value={
-                    "id": 0,
-                    "email": "user@example.com",
-                    "username": "string",
-                    "is_company": True,
-                    "company": {
-                        "id": 0,
-                        "role": "supplier",
-                        "name": "string",
-                        "inn": "string",
-                        "ogrn": "string",
-                        "company_account": "string",
-                        "phone_number": {"id": 0, "phone_number": "string"},
-                        "address": {"id": 0, "phone_number": "string"},
-                    },
-                },
-                response_only=True,
-            )
-        ],
         request=MeUserCompanyWriteSerializer,
         responses={200: MeUserCompanyReadSerializer},
         methods=["get", "put", "patch"],
-    )
-    @extend_schema(
-        description="```request body: {'current_password': 'password'}```", methods=["delete"]
     )
     @action(["get", "put", "patch", "delete"], detail=False)
     def me(self, request, *args, **kwargs):
