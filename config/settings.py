cbb69164d96c4d90627aa8098fbeb93162f73e1f
Dmitriy Ushakov Air <udv8@ya.ru>
2023-08-19 12:18:55 +0300

    Fixed Dockers run install libmagic

--- config/settings.py
+++ config/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         "NAME": os.environ["POSTGRES_DB"],
         "USER": os.environ["POSTGRES_USER"],
         "PASSWORD": os.environ["POSTGRES_PASSWORD"],
-        "HOST": "localhost",
+        "HOST": os.environ["POSTGRES_HOST"],
         "PORT": os.environ["POSTGRES_PORT"],
     },
 }
