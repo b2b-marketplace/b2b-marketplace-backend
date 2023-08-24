1bbe58ce01d31caa3df221749d08ddf6d5c49401
Olga Rotbardt <esfiro4ka@yandex.ru>
2023-08-24 03:42:38 +0300

    Правка докстринга

--- apps/baskets/permissions.py
+++ apps/baskets/permissions.py
@@ -2,7 +2,7 @@ from rest_framework import permissions
 
 
 class IsBuyer(permissions.BasePermission):
-    """Создание корзины доступно только компании-заказчику или физическому лицу."""
+    """Корзина доступна только компании-заказчику или физическому лицу."""
 
     def has_permission(self, request, view):
         if not request.user.is_authenticated:
