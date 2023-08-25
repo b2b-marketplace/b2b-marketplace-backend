b9c97d389b9ed983c99d402e3163e3088ca34fc5
bulatgabdullin <112638878+ByJIaT@users.noreply.github.com>
2023-08-25 18:16:24 +0500

    Small fixes order admin

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -5,15 +5,13 @@ from apps.orders.models import Order, OrderProduct
 
 class OrderProductInLine(admin.TabularInline):
     model = OrderProduct
+    extra = 1
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ("id", "user", "status", "created_at", "updated_at")
     search_fields = ("user",)
-    list_filter = (
-        "user",
-        "status",
-    )
+    list_filter = ("user", "status")
     empty_value_display = "-empty-"
     inlines = (OrderProductInLine,)
