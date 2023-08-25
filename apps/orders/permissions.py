51b9c26979e04e859e86d636493890b653321fe6
bulatgabdullin <112638878+ByJIaT@users.noreply.github.com>
2023-08-25 20:22:47 +0500

    Add permission conditions IsOwner

--- apps/orders/permissions.py
+++ apps/orders/permissions.py
@@ -3,7 +3,11 @@ from rest_framework import permissions
 
 class IsOwner(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.method in permissions.SAFE_METHODS or request.user.is_authenticated
+        if not request.user.is_authenticated:
+            return False
+        if request.user.id != int(view.kwargs["user_id"]):
+            return False
+        return request.user.personal or request.user.company.role == "customer"
 
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
