2dd51bdf59ad3c21ff3a26ce58ef58f4c875cb87
bulatgabdullin <112638878+ByJIaT@users.noreply.github.com>
2023-08-26 11:00:29 +0500

    Add ordering in get_related_queryset OrderManager

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -16,6 +16,7 @@ class OrderManager(models.Manager):
             .prefetch_related(
                 "orders__product", "orders__product__images", "orders__product__user__company"
             )
+            .order_by("-created_at")
         )
 
 
