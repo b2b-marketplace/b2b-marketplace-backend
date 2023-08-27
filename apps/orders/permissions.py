af3f4fd6a411a6e0f0ca8658a363583cd5efefff
bulatgabdullin <112638878+ByJIaT@users.noreply.github.com>
2023-08-27 11:22:00 +0500

    Change IsOwner

--- apps/orders/permissions.py
+++ apps/orders/permissions.py
@@ -5,8 +5,6 @@ class IsOwner(permissions.BasePermission):
     def has_permission(self, request, view):
         if not request.user.is_authenticated:
             return False
-        if request.user.id != int(view.kwargs["user_id"]):
-            return False
         return request.user.personal or request.user.company.role == "customer"
 
     def has_object_permission(self, request, view, obj):
