5bb4339941c6f1d7d61eb87a0459dbb751e18107
Ivan Skvortsov <pprofcheg@gmail.com>
2023-08-29 20:35:03 +0300

    updated conftest, fixtures for companies and users

--- apps/products/tests/conftest.py
+++ apps/products/tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 
 from apps.products.models import Category, Image, Product
-from apps.users.models import CustomUser
+from apps.users.models import Address, Company, CustomUser, PhoneNumber, PhysicalPerson
 
 
 @pytest.fixture
@@ -13,9 +13,94 @@ def guest_client():
 
 
 @pytest.fixture
-def user():
+def authorized_seller(user_seller):
+    from rest_framework.test import APIClient
+
+    client = APIClient()
+    client.force_authenticate(user=user_seller)
+    return client
+
+
+@pytest.fixture
+def authorized_buyer(user_buyer):
+    from rest_framework.test import APIClient
+
+    client = APIClient()
+    client.force_authenticate(user=user_buyer)
+    return client
+
+
+@pytest.fixture
+def authorized_physical_person(user_physical_person):
+    from rest_framework.test import APIClient
+
+    client = APIClient()
+    client.force_authenticate(user=user_physical_person)
+    return client
+
+
+@pytest.fixture
+def company_seller():
+    return Company.objects.create(
+        role="supplier",
+        name="ООО Продавец",
+        company_account=12345678901234567890,
+        inn=1234567890,
+        ogrn=1234567890123,
+        phone_number=PhoneNumber.objects.create(phone_number="+7999123456"),
+        address=Address.objects.create(address="г.Важный, ул. Неважная, дом 56"),
+    )
+
+
+@pytest.fixture
+def company_buyer():
+    return Company.objects.create(
+        role="customer",
+        name="ПАО Покупатель",
+        company_account=22345678901234567890,
+        inn=2234567890,
+        ogrn=2234567890123,
+        phone_number=PhoneNumber.objects.create(phone_number="+7999223456"),
+        address=Address.objects.create(address="г.Странный, ул. Загадочная, дом 88"),
+    )
+
+
+@pytest.fixture
+def physical_person():
+    return PhysicalPerson.objects.create(
+        first_name="Вася",
+        last_name="Халявщиков",
+        personal_account=33345678901234567890,
+        phone_number=PhoneNumber.objects.create(phone_number="+7888223456"),
+        address=Address.objects.create(address="г.Потерянный, ул. Найденная, дом 123"),
+    )
+
+
+@pytest.fixture
+def user_seller(company_seller):
     """Создает пользователя."""
-    return CustomUser.objects.create(username="username")
+    return CustomUser.objects.create(
+        username="seller", email="seller@mail.ru", is_company=True, company=company_seller
+    )
+
+
+@pytest.fixture
+def user_buyer(company_buyer):
+    """Создает пользователя."""
+    return CustomUser.objects.create(
+        username="buyer", email="buyer@mail.ru", is_company=True, company=company_buyer
+    )
+
+
+@pytest.fixture
+def user_physical_person(physical_person):
+    """Создает пользователя."""
+    return CustomUser.objects.create(
+        username="physical",
+        email="physical@mail.ru",
+        is_company=False,
+        personal=physical_person,
+    )
 
 
 @pytest.fixture
@@ -28,10 +113,10 @@ def categories():
 
 
 @pytest.fixture
-def product(user, categories):
+def product(user_seller, categories):
     """Создает продукт."""
     product = Product.objects.create(
-        user=user,
+        user=user_seller,
         category=categories[0],
         sku="123",
         name="Майка",
