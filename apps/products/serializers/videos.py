cb89ffc08202c2238d1214401103380d1f0da3c2
Dmitriy Ushakov Air <udv8@ya.ru>
2023-09-02 15:11:52 +0300

    Add validator

--- apps/products/serializers/videos.py
+++ apps/products/serializers/videos.py
@@ -1,10 +1,13 @@
 from rest_framework import serializers
 
 from apps.products.models import Video
+from apps.products.validators import validate_video
 
 
 class VideoSerializer(serializers.ModelSerializer):
     """Сериализатор для получения данных об видео."""
+
+    default_validators = [validate_video]
 
     class Meta:
         model = Video
