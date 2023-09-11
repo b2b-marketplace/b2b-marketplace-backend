5ec34d59588979da188666797c3b7d7ea1a0f99d
Dmitriy Ushakov Air <udv8@ya.ru>
2023-09-11 23:30:28 +0300

    Add video factory

--- apps/products/factories.py
+++ apps/products/factories.py
@@ -1,8 +1,9 @@
 import factory
+from django.core.files.base import ContentFile
 
 from apps.users.factories import CustomUserFactory
 
-from .models import Category, Image, Product
+from .models import Category, Image, Product, Video
 
 
 class CategoryFactory(factory.django.DjangoModelFactory):
@@ -29,7 +30,6 @@ class ProductFactory(factory.django.DjangoModelFactory):
     brand = factory.Faker("word")
     price = factory.Faker("pydecimal", min_value=0, left_digits=3, right_digits=1)
     wholesale_quantity = factory.Faker("pyint", min_value=2, max_value=10)
-    video = factory.django.FileField(filename="example.mp4")
     quantity_in_stock = factory.Faker("pyint", min_value=1, max_value=100)
     description = factory.Faker("paragraph")
     manufacturer_country = factory.Faker("country")
@@ -42,6 +42,14 @@ class ProductFactory(factory.django.DjangoModelFactory):
         if extracted:
             ImageFactory(product=self)
 
+    @factory.post_generation
+    def create_video(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            VideoFactory(product=self)
+
 
 class ImageFactory(factory.django.DjangoModelFactory):
     """Фабрика для создания изображений."""
@@ -50,4 +58,22 @@ class ImageFactory(factory.django.DjangoModelFactory):
         model = Image
 
     product = factory.SubFactory(ProductFactory)
-    image = factory.django.ImageField(filename="example.jpg")
+    image = factory.django.ImageField(filename="video_example.jpg")
+
+
+class VideoFactory(factory.django.DjangoModelFactory):
+    """Фабрика для создания видео."""
+
+    class Meta:
+        model = Video
+
+    product = factory.SubFactory(ProductFactory)
+
+    @factory.lazy_attribute
+    def video(self):
+        video_path = "apps/products/video_example/SampleVideo_360x240_200kb.mp4"
+        with open(video_path, "rb") as file:
+            video_bytes = file.read()
+
+        video_file = ContentFile(video_bytes, "video_example.mp4")
+        return video_file
