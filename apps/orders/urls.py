f8140e83a30fe92371203f7c6ed837b869738a82
bulatgabdullin <112638878+ByJIaT@users.noreply.github.com>
2023-09-12 11:49:49 +0500

    Add "supplier-orders" router

--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
-from apps.orders.views import OrderViewSet
+from apps.orders.views import OrderViewSet, SupplierOrderViewSet
 
 router = SimpleRouter()
 router.register("orders", OrderViewSet, basename="orders")
+router.register("supplier-orders", SupplierOrderViewSet, basename="supplier_orders")
 
 urlpatterns = [
     path("", include(router.urls)),
