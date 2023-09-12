e9249cf57fffa15dbf388957cf9b8399e2df46ea
bulatgabdullin <112638878+ByJIaT@users.noreply.github.com>
2023-09-12 11:49:10 +0500

    Add SupplierOrderViewSet

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -3,8 +3,12 @@ from rest_framework import permissions, status, viewsets
 from rest_framework.response import Response
 
 from apps.orders.models import Order
-from apps.orders.permissions import IsOwner
-from apps.orders.serializers.orders import OrderReadSerializer, OrderWriteSerializer
+from apps.orders.permissions import IsOwner, IsSupplier
+from apps.orders.serializers.orders import (
+    OrderReadSerializer,
+    OrderWriteSerializer,
+    SupplierOrderStatusUpdate,
+)
 
 
 class OrderViewSet(viewsets.ModelViewSet):
@@ -19,13 +23,32 @@ class OrderViewSet(viewsets.ModelViewSet):
             return OrderReadSerializer
         return OrderWriteSerializer
 
-    @extend_schema(request=OrderWriteSerializer, responses={201: OrderReadSerializer})
+    @extend_schema(request=OrderWriteSerializer, responses={201: OrderReadSerializer(many=True)})
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=[request.data], many=True)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(*serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+@extend_schema(
+    request=SupplierOrderStatusUpdate, responses={200: OrderReadSerializer}, methods=("patch",)
+)
+class SupplierOrderViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsSupplier]
+    http_method_names = ["get", "patch"]
+
+    def get_queryset(self):
+        return Order.objects.get_supplier_orders(self.request.user)
+
+    def get_serializer_class(self):
+        if self.request.method in permissions.SAFE_METHODS:
+            return OrderReadSerializer
+        return SupplierOrderStatusUpdate
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
