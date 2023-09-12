dda31092ecb3d7dbb9e640ae8526b1e8ecf2e34a
bulatgabdullin <112638878+ByJIaT@users.noreply.github.com>
2023-09-12 11:47:20 +0500

    Add IsSupplier permission

--- apps/orders/permissions.py
+++ apps/orders/permissions.py
@@ -9,3 +9,17 @@ class IsOwner(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
+
+
+class IsSupplier(permissions.BasePermission):
+    """Продавец может просматривать/редактировать заказы только со своими товарами."""
+
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+        if not request.user.is_company:
+            return False
+        return request.user.company.role == "supplier"
+
+    def has_object_permission(self, request, view, obj):
+        return obj.order_products.filter(user=request.user).exists()
