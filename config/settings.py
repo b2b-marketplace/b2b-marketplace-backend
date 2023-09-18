6fb20468731d417433e9eb6ad235415ddaa56de0
Ivan Skvortsov <pprofcheg@gmail.com>
2023-09-18 16:11:15 +0300

    changed static url: static/ -> staticfiles/, part 2 (STATCI_ROOT)

--- config/settings.py
+++ config/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "staticfiles/"
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
